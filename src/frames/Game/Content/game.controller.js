800c2cb824ac2be8a5c31b5bd16094a23c3fa512
gametrust169 <trustgamesol@gmail.com>
2024-10-27 02:23:15 -0300

    pip

--- src/frames/Game/Content/game.controller.js
+++ src/frames/Game/Content/game.controller.js
@@ -13,8 +13,8 @@ import sleep from "~/functions/sleep.js";
 // const skeletonJson = new URL('~/assets/spine/skeleton.json', import.meta.url).href;
 // const skeletonAtlas = new URL('~/assets/spine/skeleton.atlas', import.meta.url).href;
 
-import skeletonJson from '~/assets/spine/skeleton.json';
-import skeletonAtlas from '~/assets/spine/skeleton.atlas';
+const skeletonJson = '/assets/spine/skeleton.json';
+const skeletonAtlas = '/assets/spine/skeleton.atlas';
 
 const game = reactive({
     tracks: [],
